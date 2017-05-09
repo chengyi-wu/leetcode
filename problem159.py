0e67bb33c69bedd8b28433746bbe56a62d964861
chenwumsft <chengyi.wu@outlook.com>
2017-05-09 20:23:31 +0800

    Fix LRU bug

--- problem159.py
+++ problem159.py
@@ -8,10 +8,10 @@ class LRUCache(object):
 		self.htable = { }
 		self.capacity = capacity
 		self.size = 0
-		print self.check()
+		#print self.check()
 		
 	def get(self, key):
-		if key not in self.keyList:
+		if key not in self.htable:
 			return -1
 		#remove the element from the list and place it on the top of the list
 		self.keyList.remove(key)
@@ -21,10 +21,14 @@ class LRUCache(object):
 		return self.htable[key]
 		
 	def put(self, key, value):
-		if len(self.keyList) == self.capacity:
-			k = self.keyList.pop()
-			del self.htable[k]
-			self.size -= 1
+		if key in self.htable:
+			self.keyList.remove(key)
+		else:
+			if len(self.keyList) == self.capacity:
+				k = self.keyList.pop()
+				del self.htable[k]
+				self.size -= 1
+		
 		self.keyList.insert(0, key)
 		self.htable[key] = value
 		self.size += 1
@@ -41,8 +45,9 @@ def test_LRUCache():
 	
 	cache.put(1, "ITEM#1")
 	cache.put(2, "ITEM#2")
-	print cache.get(1)
+	cache.put(2, "ITEM#2")
 	cache.put(3, "ITEM#3")
+	print cache.get(1)
 	print cache.get(2)
 	
 test_LRUCache()
