8297f1f7343e09df6e4ecdd4016c6d7623fa4bc3
chenwu <chengyi.wu@outlook.com>
2017-05-23 13:15:48 +0800

    +121

--- problem121.py
+++ problem121.py
@@ -1,4 +1,6 @@
 '''
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/#/description
+
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
