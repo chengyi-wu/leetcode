ff5a2e18e3bf230a545c5927a4dedfa530903788
chenwumsft <chengyiwu@Chengyis-MacBook-Pro.local>
2017-05-23 22:11:48 +0800

    +2 DP

---
+++ editdistance.py
@@ -0,0 +1,4 @@
+'''
+https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/lecture-21-dp-iii-parenthesization-edit-distance-knapsack/
+
+'''
---
+++ textjustification.py
@@ -0,0 +1,6 @@
+'''
+https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/lecture-20-dynamic-programming-ii-text-justification-blackjack/
+
+Subproblems: Where the 2nd line begins. (word[i:])
+
+'''
