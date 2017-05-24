2d9d4f7a43612ec4214b98a7357239fd4c7f2a34
chenwumsft <chengyiwu@Chengyis-MacBook-Pro.local>
2017-05-24 22:15:41 +0800

    tj

--- textjustification.py
+++ textjustification.py
@@ -2,42 +2,37 @@
 https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/lecture-20-dynamic-programming-ii-text-justification-blackjack/
 
 Subproblems: Where the 2nd line begins. (word[i:])
-
 '''
 import math
 import sys
-def badness(words, i, j, maxWidth):
+def badness(line, maxWidth):
     w = 0
-    for z in range(i, j):
-        w += len(words[z])
+    for word in line:
+        w += len(word)
     if w > maxWidth:
-        return sys.maxint
-    return (maxWidth - w) * (maxWidth - w) * (maxWidth - w)
+        return float("inf")
+    v = (maxWidth - w) * (maxWidth - w) * (maxWidth - w)
+    print line, v
+    return v
 
 def fullJustify(words, maxWidth):
-    if len(words) == 1:
-        min_badness = badness(words, 0, 1, maxWidth)
-        return (min_badness, [words[0]])
-    for i in range(1, len(words)):
-        #The badness for this line
-        b = badness(words, 0, i, maxWidth)
-        if b == sys.maxint:
-            break
-        else:
-            n, lines = fullJustify(words[i:], maxWidth)
-            if i == 1:
-                min_badness = b + n
-                line_break = i
-            else:
-                if b + n < min_badness:
-                    min_badness = b + n
-                    line_break = i
-
-    lines = [' '.join(words[:line_break])] + lines
+    #print words
+    if len(words) == 0:
+        return (0, [])
+    line = words[:1]
+    min_badness, rest_lines = fullJustify(words[1:], maxWidth)
+    min_badness += badness(line, maxWidth)
+    lines = [line] + rest_lines
+    for i in range(2, len(words) + 1):
+        line = words[:i]
+        rest_badness, rest_lines = fullJustify(words[i:], maxWidth)
+        if badness(line, maxWidth) + rest_badness < min_badness:
+            min_badness = badness(line, maxWidth) + rest_badness
+            lines = [line] + rest_lines
     return (min_badness, lines)
 
-def test_fullJustify(words, maxWidth):
-    for line in fullJustify(words, maxWidth)[1]:
-        print line
 
-test_fullJustify(["This", "is", "an", "example", "of", "text", "justification.", "This", "is", "my", "test."], 16)
+def test_fullJustify(words, maxWidth):
+    print fullJustify(words, maxWidth)
+
+test_fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16)
