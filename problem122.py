089a28bea6bc8c8da703b6829678578b98c13a9e
chenwu <chengyi.wu@outlook.com>
2017-05-27 09:54:57 +0800

    +122

--- problem122.py
+++ problem122.py
@@ -2,19 +2,32 @@ def helper(prices, i, memo):
     if i in memo:
         return memo[i]
     profit = 0
-    for j in range(i + 1, len(prices) - 1):
+    for j in range(i, len(prices)):
         profit = max(profit, prices[j] - prices[i] + helper(prices, j + 1, memo))
     memo[i] = profit
+    print prices[i:], profit
     return profit
 
 def maxProfit(prices):
+    '''
+    This time out as well as STACK OVERFLOW
+    '''
     maxProfit = 0
     memo = { }
-    for i in range(len(prices) - 1):
+    for i in range(len(prices)):
         maxProfit = max(helper(prices, i, memo), maxProfit)
     return maxProfit
 
-def test_maxProfit(prices):
-    print maxProfit(prices)
+def maxProfit2(prices):
+    profits = prices[:]
+    for i in range(len(prices)):
+        if i == 0:
+            profits[i] = 0
+        else:
+            profits[i] = max(0, prices[i] - prices[i - 1])
+    return sum(profits)
 
-test_maxProfit([7,1,5,3,6,4])
+def test_maxProfit(prices):
+    print maxProfit2(prices)
+
+test_maxProfit([3,2,6,5,0,3])
