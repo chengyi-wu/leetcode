7339cf4cc0a244cf6a0ae6cb137d272da5022e8b
chenwu <chengyi.wu@outlook.com>
2017-06-09 11:03:46 +0800

    +306
    Recursion won't work for large input. Rewrite the code to iteration.
    https://leetcode.com/submissions/detail/105445453/

--- problem306.py
+++ problem306.py
@@ -15,8 +15,39 @@ def isAdditiveNumber(path, num):
             return True
     return False
 
+def helper(stack, num):
+    #print stack, num
+    while len(num) > 0:
+        print stack, num
+        n = str(stack[-1] + stack[-2])
+        l = len(n)
+        if n == num[:l]:
+            stack.append(int(n))
+        else:
+            return False
+        num = num[l:]
+    return len(stack) > 2
+
+
+def isAdditiveNumber2(num):
+    '''
+    Rewrite the recursion to iteration.
+    '''
+    for i in range(1, len(num)):
+        n = int(num[:i])
+        if str(n) == num[:i]:
+            stack = [n]
+            for j in range(i + 1, len(num)):
+                n = int(num[i:j])
+                if str(n) == num[i:j] and helper(stack + [n], num[j:]) == True:
+                    return True
+    return False
+
 def test():
-    print isAdditiveNumber([], "112358")
-    print isAdditiveNumber([], "199100199")
+    print isAdditiveNumber2("0235813")
+    print isAdditiveNumber2("101")
+    print isAdditiveNumber2("1023")
+    print isAdditiveNumber2("112358")
+    print isAdditiveNumber2("199100199")
 
 test()
