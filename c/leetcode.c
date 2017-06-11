d540af74b2d3a2a412c1f0b085c6c8430ec3d997
chenwumsft <chengyiwu@Chengyis-MacBook-Pro.local>
2017-06-11 21:38:33 +0800

    +309
    N^2 algorithm

---
+++ c/leetcode.c
@@ -0,0 +1,9 @@
+#include "leetcode.h"
+#include "problem309.c"
+
+int main(int argc, char* argv[])
+{
+	test();
+
+	return 0;
+}
---
+++ c/leetcode.h
@@ -0,0 +1,10 @@
+#ifndef __LEETCODE_H
+#define __LEETCODE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define MIN(a,b) ((a)>(b))?(b):(a)
+#define MAX(a,b) ((a)>(b))?(a):(b)
+
+#endif
