34b82623a7f6dc40aecc73ce6bfe4b7f4d11d7d6
chenwu <chengyi.wu@outlook.com>
2017-07-03 15:58:59 +0800

    +128
    https://leetcode.com/problems/longest-consecutive-sequence/#/description
    
    https://leetcode.com/submissions/detail/108170108/

--- problem128.py
+++ problem128.py
@@ -17,8 +17,40 @@ def longestConsecutive(nums):
             maxlen = max(maxlen, currentlen)
     return maxlen
 
+def longestConsecutive2(nums):
+    small = { }
+    big = { }
+    for n in nums:
+        small[n] = None
+        big[n] = None
+    for k in nums:
+        if k - 1 in small:
+            small[k] = k - 1
+        if k + 1 in big:
+            big[k] = k + 1
+    #h = { }
+    h = []
+    maxlen = 0
+    #print small,big
+    for n in nums:
+        currentlen = 1
+        if n not in h:
+            x = small[n]
+            while x != None:
+                currentlen += 1
+                #h[x] = True
+                h.append(x)
+                x = small[x]
+            x = big[n]
+            while x != None:
+                currentlen += 1
+                #h[x] = True
+                h.append(x)
+                x = big[x]
+        maxlen = max(maxlen, currentlen)
+    return maxlen
 
 def test():
-    print longestConsecutive([2147483647, 4, 200, 1, 3, 2])
+    print longestConsecutive2([2147483647, 4, 200, 1, 3, 2])
 
 test()
