f9149d6b1e77f96ca49535516a61ce24709bc461
chenwu <chengyi.wu@outlook.com>
2017-07-08 18:58:56 +0800

    +378
    using list comprehension

--- problem378.py
+++ problem378.py
@@ -4,10 +4,8 @@ def kthSmallest(matrix, k):
     '''
     row = len(matrix)
     col = len(matrix[0])
-    nums = []
-    for i in range(row):
-        for j in range(col):
-            nums.append((matrix[i][j], i * col + j ))
+    #list comprehension is much faster
+    nums = [(matrix[i][j], i * col + j ) for i in range(row) for j in range(col)]
     nums.sort(key=lambda x:x[0])
     
     n = nums[k - 1][1]
