48b908c2ba6f005c27711d831de1f6922fa7d0c2
chengyimbp <chengyiwu@Chengyis-MacBook-Pro.local>
2017-07-09 21:45:13 +0800

    +23v2
    Why not TLE?

---
+++ problem23v2.py
@@ -0,0 +1,36 @@
+def merge(l1, l2):
+    head = None
+    p1 = l1
+    p2 = l2
+    p = None
+    while p1 is not None or p2 is not None:
+        if p1 is None:
+            p.next = p2
+            return head
+        if p2 is None:
+            p.next = p2
+            return head
+        
+        if p1.val < p2.val:
+            node = p1
+            p1 = p1.next
+        else:
+            node = p2
+            p2 = p2.next
+        if head is None:
+            head = node
+        else:
+            p.next = node
+        p = node
+    return head
+
+def mergeKLists(lists):
+    '''
+    :type lists: List[ListNode]
+    :rtype ListNode
+    '''
+    k = len(lists)
+    while k > 1:
+        l = lists.append(lists.pop(), lists.pop())
+        k -= 1
+    return lists[0]
